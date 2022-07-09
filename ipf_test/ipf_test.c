b1ab4230272c86b0627a218ef61e7046f84c5211
Mike Heinen <mbheinen@gmail.com>
2022-07-08 22:28:48 -0500

    Fixing compile error

--- ipf_test/ipf_test.c
+++ ipf_test/ipf_test.c
@@ -20,24 +20,3 @@ main( int argc, char *argv[] )
     pf_cmd ();
 #endif
 }
-#if defined VMS
-   void fstout( )
-{
-  fprintf (stderr, "IPF cannot execute /OUTAGE_SIMULATION\n");
-}
-#elif defined UNDERSCORE
-   void fstout_( )
-{
-  fprintf (stderr, "IPF cannot execute /OUTAGE_SIMULATION\n");
-}
-#elif defined DUNDERSCORE
-   void fstout__( )
-{
-  fprintf (stderr, "IPF cannot execute /OUTAGE_SIMULATION\n");
-}
-#else
-   void fstout( )
-{
-  fprintf (stderr, "IPF cannot execute /OUTAGE_SIMULATION\n");
-}
-#endif
